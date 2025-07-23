879743937d6ecef0924d0bbe17fdeacb816a18d5
hassan-memon-09 <muhammadhassan.pitp@gmail.com>
2025-07-23 18:03:08 +0500

    Update

--- my-app/src/main.jsx
+++ my-app/src/main.jsx
@@ -2,9 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
-import "bootstrap/dist/css/bootstrap.min.css"; // Bootstrap CSS
-import "bootstrap/dist/js/bootstrap.bundle.min.js"; // Bootstrap JS (includes Popper)
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
